daa9ce106fc70759a6b2a667af8cd5f7ccd49018
Luan Fernandes <luan@escribo.com>
2022-06-28 11:04:38 -0300

    route manager for backend api

--- BackEnd/index.php
+++ BackEnd/index.php
@@ -1,8 +1,19 @@
 <?php
 
-$dados = file_get_contents("php://input");
-$dados = json_decode($dados);
-$dados['status'] = 'ok';
-$dados['id'] = '1';
-$dados = json_encode($dados);
-echo $dados;
+$router = $_GET['router'];
+$router = explode('/', $router);
+
+$postData = file_get_contents("php://input");
+
+//$router[0] = onde fazer
+//$router[1] = ação para fazer
+
+require_once 'App/Services/'.strtolower($router[0]).'.php';
+
+$service = $router[1];
+
+if (isset($router[2])) {
+    $router[0]::$service($router[2], $postData);
+}else{
+    $router[0]::$service('', $postData);
+}
